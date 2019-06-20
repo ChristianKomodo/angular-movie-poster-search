a1d55c7622c6c16f48287bb8bba004afdecfa485
703114283 <christian.knoll@universalorlando.com>
2019-06-20 16:47:04 -0400

    finds movie by search and displays poster

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,22 +9,38 @@ import { DataService } from './data.service';
 export class AppComponent {
   title = 'Multiple HTTP get() Methods';
   movie: Object;
+  movieSearch: Object;
+  searchTermEntered: string;
+  // foundMovies: any[];
+  foundMovies: Object;
+  foundMovieTitle: string;
+  foundMovieID: string;
+  foundMoviePosterURL: string;
 
-  constructor(private data: DataService) {}
+  constructor(private data: DataService) { }
 
   ngOnInit() {
-    this.data.getMovie().subscribe(data => {
+    this.data.getMovie('tt0437086').subscribe(data => {
       this.movie = data;
-      console.log('this.movie is', data);
+      console.log('this.movie is', this.movie);
     });
   }
 
-  onKeyUp($event: any) {
-    if ($event.keyCode === 13) {
-      console.log('Enter pressed.');
-    } else {
-      console.log($event.key);
-    }
+  onMovieSearch(searchTerm) {
+    this.data.getMovieBySearch(searchTerm).subscribe(data => {
+      this.foundMovies = data;
+      console.log('this.foundMovies is', this.foundMovies);
+      this.foundMovieTitle = this.foundMovies.Search[0].Title;
+      this.foundMovieID = this.foundMovies.Search[0].imdbID;
+      this.foundMoviePosterURL = this.foundMovies.Search[0].Poster;
+      console.log('this.foundMovieID is', this.foundMovieID);
+    });
+  }
+
+  onSearchEntered(event: any) {
+    this.searchTermEntered = event.target.value;
+    // console.log('search term is now', this.searchTermEntered);
+    this.onMovieSearch(this.searchTermEntered);
   }
 
 }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -9,7 +9,11 @@ export class DataService {
 
   constructor(private http: HttpClient) { }
 
-  getMovie() {
-    return this.http.get('https://reqres.in/api/users/1');
+  getMovieBySearch(searchTerm: string) {
+    return this.http.get(`https://www.omdbapi.com/?s=${searchTerm}&page=1&type=movie&apikey=9baa487f`);
+  }
+
+  getMovie(movieID: string) {
+    return this.http.get(`http://www.omdbapi.com/?i=${movieID}&plot=short&apikey=9baa487f`);
   }
 }
